9058dad9ecd3c6d8e07121ba9c2aa872d113b334
Rain Rahni <rain.rahni5@gmail.com>
2024-01-08 10:47:00 +0200

    create training layers and also controller and service layer

--- src/main/java/com/kooos/kooos/model/Training.java
+++ src/main/java/com/kooos/kooos/model/Training.java
@@ -1,11 +1,9 @@
 package com.kooos.kooos.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,29 +11,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "users")
-public class User {
+@Table(name = "trainings")
+public class Training {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userName;
-    private String email;
-    private String password;
-    @ManyToMany(
-            cascade = CascadeType.ALL
-    )
-    private List<Meal> meals;
-    @ManyToMany(
-            cascade = CascadeType.ALL
-    )
-    private List<Training> trainings;
-
+    private String name;
+    //TODO: SAME as with meal, decide whether to use type as enum or store in database.
+    private String type;
+    private int durationInMinutes;
+    private int durationInSeconds;
 }
---
+++ src/main/java/com/kooos/kooos/service/TrainingService.java
@@ -0,0 +1,9 @@
+package com.kooos.kooos.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrainingService {
+}
