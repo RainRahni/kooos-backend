43d7e5fa215380d8ec2aa387a8237b64730d02d8
Rain Rahni <rain.rahni5@gmail.com>
2024-01-08 10:30:43 +0200

    initial gradle push and user,food and meal model

---
+++ src/main/java/com/kooos/kooos/KooosApplication.java
@@ -0,0 +1,14 @@
+package com.kooos.kooos;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KooosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KooosApplication.class, args);
+	}
+
+}
